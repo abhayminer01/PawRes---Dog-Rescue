56c34d332bdf81a35bb05f12ec591738f4b358b0
abhayminer01 <abhayvapvl@gmail.com>
2025-08-19 23:51:19 +0530

    Login Setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,14 @@
 const User = require("../models/User.model");
 
 const userLogin = async (req, res) => {
+    const { email, password } = req.body;
     try {
-        
+        const user = await User.findOne({ email : email });
+        if(!user) {
+            return res.json({ success : false, message : "user not found" });
+        }
+
+        res.json({ success : true });
     } catch (error) {
         console.log(`Error Occured while user login : ${error}`);
         res.status(400).json({ success : false, message : 'Error occured', err : error });
@@ -12,7 +18,19 @@ const userLogin = async (req, res) => {
 const userregistration = async (req, res) => {
     try {
         const { name, email, password } = req.body;
-        const existingUser = await User.findOne({ email })
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            return res.json({ success : false })
+        }
+
+        const user = new User({
+            email : email,
+            password : password,
+            name : name
+        });
+        await user.save();
+
+        res.json({ success : true });
     } catch (error) {
         console.log(`Error occured while user registration : ${error}`);
         res.status(400).json({ success : false, message : 'Error occured', err : error });
@@ -20,5 +38,6 @@ const userregistration = async (req, res) => {
 }
 
 module.exports = {
-    userLogin
+    userLogin,
+    userregistration
 }
